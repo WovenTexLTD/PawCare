b61de263ee3e1dd4e93d174f9ed05f832e69eb57
Karim Sabbagh <karimsabbagh@woventex.co>
2025-12-13 14:57:47 +0100

    Update layout.tsx

--- pawcare-website/src/app/layout.tsx
+++ pawcare-website/src/app/layout.tsx
@@ -3,6 +3,22 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Pacifico } from "next/font/google";
+import Header from "@/components/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
 
 const pawcareFont = Pacifico({
   subsets: ["latin"],
